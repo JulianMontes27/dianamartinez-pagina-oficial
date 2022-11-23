769bd46bea83595dd8a2acf61a32c8616176ae37
Julian Montes <julianmontesPS4@gmail.com>
2022-11-23 18:38:56 -0500

    Folder structure done

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './navbar.css';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+
+//BEM -> BLOCK ELEMENT MODIFIER
+
+const NavBar = () => {
+  return (
+    <div className='app__navbar'>
+      <div className='app_navbar-links'>
+        <div className='app__navbar-links_logo'>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
+
